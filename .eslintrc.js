fa5a918182c1a83ced541a5200a09f5c330f94de
jackzhou <jackzhou@yeahka.com>
2022-05-23 16:11:42 +0800

    添加eslint、.prettierrc等

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/essential',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {},
+};
